9443d8c3ed98a93e29be2296fd9cafb51f2c9fb4
senthil-cit-erode <senthil.cit.erode@gmail.com>
2016-03-18 13:32:49 +0530

    Update WordSearch.java

--- src/main/java/com/freecharge/problem/wordcount/controller/WordSearch.java
+++ src/main/java/com/freecharge/problem/wordcount/controller/WordSearch.java
@@ -46,7 +46,7 @@ public class WordSearch {
 			}
 		} else {
 			if(folder.getAbsolutePath().endsWith(".txt")){
-				System.out.println("Adding file : "folder.getAbsolutePath());
+				System.out.println("Adding file : "+folder.getAbsolutePath());
 				loadMap(folder);
 			} else {
 				System.out.println("Skipping file : "+folder.getAbsolutePath());
